4d455d41f42ba938e8dcd1be3311b2c2a795fa80
muhammad-huzaifah <huzaifah2009@gmail.com>
2022-09-13 05:15:59 +0700

    learn crud

--- application/views/siswa/create.php
+++ application/views/siswa/create.php
@@ -27,8 +27,26 @@
 				</div>
 				
 				<div class="form-group">
-					<label for=""></label>
+					<label for="tempat_lahir">Tempat Lahir</label>
+                    <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir">
 				</div>
+
+                <div class="form-group">
+                    <label for="tgl_lahir">Tanggal Lahir</label>
+                    <input type="text" class="form-control datepicker" id="tanggal_lahir" name="tanggal_lahir">
+                </div>
+
+                <div class="form-group">
+                    <label for="no_telp">No Telp</label>
+                    <input type="number" class="form-control" id="no_telp" name="no_telp">
+                </div>
+
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <textarea class="form-control" name="alamat" id="alamat" ></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
 			</form>
 		</div>
 	</div>
