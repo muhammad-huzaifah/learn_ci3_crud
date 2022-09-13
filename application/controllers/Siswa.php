e77b752f92a18c8ca7f74ccd223f7de62862efb4
ujebb <ujebbawas@yahoo.com>
2022-09-13 16:27:12 +0700

    learn method save and edit

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -24,4 +24,39 @@ class Siswa extends CI_Controller
 		$this->load->view('template/footer');
 	}
 
+    public function save()
+    {
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('jenis_kelamin', 'Jenis Kelamin', 'required');
+        $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+        $this->form_validation->set_rules('tanggal_lahir', 'Tanggal Lahir', 'required');
+        $this->form_validation->set_rules('no_telp', 'Nomor Telepon', 'required');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+        if ($this->form_validation->run() == true)
+        {
+            $data['nama'] = $this->input->post('nama');
+            $data['jenis_kelamin'] = $this->input->post('jenis_kelamin');
+            $data['tempat_lahir'] = $this->input->post('tempat_lahir');
+            $data['tanggal_lahir'] = $this->input->post('tanggal_lahir');
+            $data['no_telp'] = $this->input->post('no_telp');
+            $data['alamat'] = $this->input->post('alamat');
+            $this->siswa_model->save($data);
+            redirect('siswa');
+        }
+        else
+        {
+            $this->load->view('template/header');
+            $this->load->view('siswa/create');
+            $this->load->view('template/footer');
+        }
+    }
+
+    function edit($kd_siswa)
+    {
+        $data['siswa'] = $this->siswa_model->getById($kd_siswa);
+        $this->load->view('template/header');
+        $this->load->view('siswa/edit', $data);
+        $this->load->view('template/footer');
+    }
+
 }
--- application/models/Siswa_model.php
+++ application/models/Siswa_model.php
@@ -9,4 +9,14 @@ class Siswa_model extends CI_Model
 	{
 		return $this->db->get($this->table)->result();
 	}
+
+    public function save($data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
+
+    public function getById($id)
+    {
+        return $this->db->get_where($this->table, ["kd_siswa" => $id])->row();
+    }
 }
