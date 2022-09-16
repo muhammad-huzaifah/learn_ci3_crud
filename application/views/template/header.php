5cbd4b224998a6f1b08fc42949f1835a6d0c6cef
ujebb <ujebbawas@yahoo.com>
2022-09-16 16:46:18 +0700

    change header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,8 +5,8 @@
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Starter Template for Bootstrap</title>
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker3.min.css">
+	<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker3.min.css" rel="stylesheet">
 	<script src="<?php echo base_url(); ?>assets/js/jquery-3.5.0.min.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.min.js"></script>
